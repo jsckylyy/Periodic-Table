994a787671a1668d2b128253781dbf6aee22df9e
jsckylyy <79430245+jsckylyy@users.noreply.github.com>
2024-05-20 20:58:00 -0400

    Initial Committ

---
+++ ReplicatedStorage/Table/Directory.lua
@@ -0,0 +1,23 @@
+local Directory = {
+    [1] = {
+        Abbreviation = "H",
+        ElementName = "Hydrogen",
+        AtomicNumber = 1, 
+        AtomicMass = 1.0080,
+        ElementGroup = "Nonmetal",
+        Group = 1,
+        Period = 1,
+    },
+
+    [2] = {
+        Abbreviation = "He"
+        ElementName = "Helium",
+        AtomicNumber = 1, 
+        AtomicMass = 1.0080,
+        ElementGroup = "Nonmetal",
+        Group = 1,
+        Period = 1,
+    },
+}
+
+return Directory
