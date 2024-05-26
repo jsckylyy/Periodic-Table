7ccb421bca77a821bc5dfe6d08ab99c82a2ce3c4
jsckylyy <envinius@gmail.com>
2024-05-25 21:59:21 -0400

    source commit chat

--- ReplicatedStorage/Main/Init.lua
+++ ReplicatedStorage/Main/Init.lua
@@ -1,81 +1,163 @@
+--!nocheck
 local Init = {}
---#SERVICE CODE 
+
+--//@Services 
+local Services = setmetatable({}, {
+	__index = function(self, Service)
+		return game:GetService(Service)
+	end,
+})
+--//ServiceDiplomacies 
+local Players = Services.Players
+local TweenService, Tweens = Services.TweenService, Services.TweenService 
+local UserInputService = Services.UserInputService 
+local RunService = Services.RunService 
+
+--//@Start Dependencies
+local Player = game.Players.LocalPlayer 
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Camera = workspace.CurrentCamera
+
+--//@Legacy Abbreviations 
+local mouse : Mouse = Player:GetMouse() 
+
+--//@Local Functions 
+local function zoomCam(amount : number , duration : number) 
+	local tween = Tweens:Create(
+		Camera,
+		(TweenInfo.new)(duration), 
+		{FieldOfView = amount}
+	)
+	tween:Play() 
+	task.delay(duration - -(math.cos(10)), tween.Destroy, tween) 
+end
+
+local function Tween(instance : any, durationid : number, prop : string, value : any)
+	local tween = Tweens:Create(
+		instance, 
+		TweenInfo.new(durationid), 
+		{[prop] = value}
+	)
+	tween:Play() 
+	task.delay(durationid + .5, tween.Destroy, tween)
+end
+
+function Rotate(player, mouse)
+	if mouse then 
+		--/Rotate 
+		local bg = Instance.new("BodyGyro")
+		bg.Name = "Rotate"
+		bg.Parent = player.Character.PrimaryPart
+		bg.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
+		bg.P = 10000000
+		bg.D = 10000
+		local mouseLocation = Vector3.new(mouse.X, player.Character.PrimaryPart.Position.Y, mouse.Z)
+		bg.CFrame = CFrame.new(player.Character.HumanoidRootPart.Position, mouseLocation)
+		task.delay(.2, bg.Destroy, bg)	
+	end
+end
+
+
+local function getMouseHit()
+	local raycastParams = RaycastParams.new()
+	raycastParams.FilterType = Enum.RaycastFilterType.Exclude
+	raycastParams.FilterDescendantsInstances = {Character, workspace.Debris}
+	local mouseLocation = UserInputService:GetMouseLocation()
+
+	local viewportPointRay = Camera:ViewportPointToRay(mouseLocation.X, mouseLocation.Y)
+	local RayResult = workspace:Raycast(viewportPointRay.Origin, viewportPointRay.Direction * 1000, raycastParams)
+	if RayResult then
+		return RayResult.Instance 
+	end
+end
+
+local function getMousePos()	
+	local raycastParams = RaycastParams.new()
+	raycastParams.FilterType = Enum.RaycastFilterType.Exclude
+	raycastParams.FilterDescendantsInstances = {Character, workspace.Debris}
+
+	local mouseLocation = UserInputService:GetMouseLocation()
+	local viewportPointRay = Camera:ViewportPointToRay(mouseLocation.X, mouseLocation.Y)
+	local RayResult = workspace:Raycast(viewportPointRay.Origin, viewportPointRay.Direction * 1000, raycastParams)
+	if RayResult then
+		return RayResult.Position
+	end
+	--//Shift/Viewport Ray @: return nil, (viewportPointRay.Origin + viewportPointRay.Direction * 1000)
+end 
 
 local Dictionary : any = require(game.ReplicatedStorage.Dictionaries.Elements)
 local ElementIndex : any = require(game.ReplicatedStorage.Dictionaries.Index)
 
-local function Tween(Instance, Duration, Property, Value): ?
-end
+local function ApplyAttributes(Target : any, ElementNumber : number)
+	Target:SetAttribute("Symbol", Dictionary[ElementNumber].Symbol)
+	Target:SetAttribute("ElementName", Dictionary[ElementNumber].ElementName)
+	Target:SetAttribute("AtomicNumber", Dictionary[ElementNumber].AtomicNumber)
+	Target:SetAttribute("Description", Dictionary[ElementNumber].Description)
 
-local ApplyAttributes(Target : any, ElementNumber : number): ?
-    Target:SetAttribute("Symbol", Dictionary[ElementNumber].Symbol)
-    Target:SetAttribute("ElementName", Dictionary[ElementNumber].ElementName)
-    Target:SetAttribute("AtomicNumber", Dictionary[ElementNumber].AtomicNumber)
-    Target:SetAttribute("Description", Dictionary[ElementNumber].Description)
+	Target:SetAttribute("Group", Dictionary[ElementNumber].Group)
+	Target:SetAttribute("Period", Dictionary[ElementNumber].Period)
 
-    Target:SetAttribute("Group", Dictionary[ElementNumber].Group)
-    Target:SetAttribute("Period", Dictionary[ElementNumber].Period)
-
-    Target:SetAttribute("AtomicMass", Dictionary[ElementNumber].AtomicMass)
-    Target:SetAttribute("ElementGroup", Dictionary[ElementNumber].ElementGroup)
+	Target:SetAttribute("AtomicMass", Dictionary[ElementNumber].AtomicMass)
+	Target:SetAttribute("ElementGroup", Dictionary[ElementNumber].ElementGroup)
 end
 
 function Init.ClientListener()
-    local Player : Player = game.Players.LocalPlayer 
-    local Character : Character = Player.Character or Player.CharacterAdded:Wait() 
-    local GUI : ScreenGui = Player.PlayerGui:FindFirstChild("PeriodicTabe")
-    
-    --//Initial Load GUI
-    for _, v in(GUI:GetDescendants()) do 
-        if v:IsA("Frame") then 
-            v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
-            v.BackgroundTransparency = 1 
-        elseif v:IsA("ImageLabel") or v:IsA("ImageButton") then
-            v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
-            v:SetAttribute("ImageTransparency", v.ImageTransparency) 
-            v.ImageTransparency = 1 
-            v.BackgroundTransparency = 1
-        elseif v:IsA("TextLabel") or v:IsA("TextButton") then 
-            v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
-            v:SetAttribute("TextTransparency", v.TextTransparency) 
-            v.TextTransparency = 1 
-            v.BackgroundTransparency = 1
-        end
-    end
+	local Player : Player = game.Players.LocalPlayer 
+	local Character : Character = Player.Character or Player.CharacterAdded:Wait() 
+	local GUI : ScreenGui = Player.PlayerGui:WaitForChild("Table")
 
-    for _, v in(GUI.InteractiveElements:GetChildren()) do 
-        if v.Name == Dictionary[v.Name] then 
-            --//#ASSUMING v:IsA("TextLabel") and v:IsADescendantOf("GUI") >> true
-            local ElementNumber : number = tonumber(v.Name)
-            v.Symbol.Text = Dictionary[ElementNumber].Symbol
-            v.AtomicMass.Text = Dictionary[ElementNumber].AtomicMass
-            v.ElementName.Text = Dictionary[ElementNumber].ElementName
-            --
+	--//Initial Load GUI
+	for _, v in(GUI:GetDescendants()) do 
+		if v:IsA("Frame") then 
+			v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
+			v.BackgroundTransparency = 1 
+		elseif v:IsA("ImageLabel") or v:IsA("ImageButton") then
+			v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
+			v:SetAttribute("ImageTransparency", v.ImageTransparency) 
+			v.ImageTransparency = 1 
+			v.BackgroundTransparency = 1
+		elseif v:IsA("TextLabel") or v:IsA("TextButton") then 
+			v:SetAttribute("BackgroundTransparency", v.BackgroundTransparency)
+			v:SetAttribute("TextTransparency", v.TextTransparency) 
+			v.TextTransparency = 1 
+			v.BackgroundTransparency = 1
+		end
+	end
 
-            for index, value in(Dictionary[ElementNumber]) do 
-                v:SetAttribute(index, value)
-            end
+	for i, _ in(Dictionary) do 
+		if tostring(i) == GUI.Frame.InteractiveElements:FindFirstChild(i).Name then 
+			--//#ASSUMING v:IsA("TextLabel") and v:IsADescendantOf("GUI") >> true
+			local v = GUI.Frame.InteractiveElements[i]
+			local ElementNumber : number = tonumber(v.Name)
+			v.Symbol.Text = Dictionary[ElementNumber].Symbol
+			v.ElementName.Text = Dictionary[ElementNumber].ElementName
+            v.AtomicNumber.Text = Dictionary[ElementNumber].AtomicNumber
+			--
 
-            if v:GetAttribute("Symbol") != Dictionary[ElementNumber] then 
-                --//#FAIL SAFE
-               ApplyAttributes(v, ElementNumber)
-            end
+			for index, value in(Dictionary[ElementNumber]) do 
+				v:SetAttribute(index, value)
+			end
 
-            v.Activated:Connect(function() 
-                warn(v.Name .. " selected");
-            end)
+			if v:GetAttribute("Symbol") ~= Dictionary[ElementNumber] then 
+				--//#FAIL SAFE
+				ApplyAttributes(v, ElementNumber)
+			end
 
-            v.MouseEnter:Connect(function() 
-                Tween(v, 1, "BackgroundColor", Color3.fromRGB(255,255,255)); --TODO: Make this a gray color
-                Tween(v.ElementName, .5, "TextTransparency", 0);
-            end)
+			v.Activated:Connect(function() 
+				warn(v.Name .. " selected");
+			end)
 
-            v.MouseLeave:Connect(function() 
-                Tween(v, 1, "BackgroundColor", Index[Dictionary[ElementNumber].ElementGroup].Color);
-                Tween(v.ElementName, .5, "TextTransparency", 1);
-            end)
-        end
-    end
+			v.MouseEnter:Connect(function() 
+				Tween(v, 1, "BackgroundColor", Color3.fromRGB(147, 147, 147)); --TODO: Make this a gray color
+				Tween(v.ElementName, .5, "TextTransparency", 0);
+			end)
+
+			v.MouseLeave:Connect(function() 
+				Tween(v, 1, "BackgroundColor", ElementIndex[Dictionary[ElementNumber].ElementGroup].Color);
+				Tween(v.ElementName, .5, "TextTransparency", 1);
+			end)
+		end
+	end
 
 end
 
